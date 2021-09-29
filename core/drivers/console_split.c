7f707cbbdbcfbf5b64240980ef5532c58852ed79
Tom Van Eyck <tom.vaneyck@telenet.be>
2021-09-29 13:13:44 +0200

    Character device

--- core/drivers/console_split.c
+++ core/drivers/console_split.c
@@ -3,11 +3,10 @@
 #include <kernel/pseudo_ta.h>
 #include <kernel/interrupt.h>
 #include <kernel/notif.h>
-
-#define NOTIF_INTERRUPT 260
+#include <string.h>
 
 static struct serial_chip *serial_chip_con_split;
-static enum itr_return console_split_itr_cb(void);
+static enum itr_return console_split_itr_cb(struct itr_handler *handler __unused);
 static uint32_t notif_value = -1;
 
 static struct itr_handler console_itr = {
@@ -23,13 +22,13 @@ void register_serial_chip_con_split(struct serial_chip *chip)
 	serial_chip_con_split = chip;
 }
 
-static enum itr_return console_split_itr_cb(void)
+static enum itr_return console_split_itr_cb(struct itr_handler *handler __unused)
 {
 	// Send notification that a serial interrupt was registered.
 	// Disable interrupt in the meantime.
 	itr_disable(console_itr.it);
 	IMSG("Raising interrupt.\n");
-	itr_raise_pi(NOTIF_INTERRUPT);
+	notif_send_async(notif_value);
 
 	return ITRR_HANDLED;
 }
@@ -60,6 +59,27 @@ static TEE_Result read_char(uint32_t ptypes, TEE_Param params[TEE_NUM_PARAMS])
 
 	// Re-enable interrupt
 	itr_enable(console_itr.it);
+
+	return TEE_SUCCESS;
+}
+
+static TEE_Result write_chars(uint32_t ptypes, TEE_Param params[TEE_NUM_PARAMS])
+{
+	uint32_t expected_ptypes =
+		TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,
+				TEE_PARAM_TYPE_NONE, TEE_PARAM_TYPE_NONE,
+				TEE_PARAM_TYPE_NONE);
+
+	if (ptypes != expected_ptypes)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	char *buf = params[0].memref.buffer;
+	uint32_t size = params[0].memref.size;
+	char local[size];
+
+	memcpy(local, buf, size);
+
+	IMSG("Received %s\n", local);
 
 	return TEE_SUCCESS;
 }
@@ -101,7 +121,7 @@ static TEE_Result register_itr(uint32_t ptypes,
 }
 
 static TEE_Result unregister_itr(uint32_t ptypes,
-				 TEE_Param params[TEE_NUM_PARAMS])
+				 TEE_Param params[TEE_NUM_PARAMS] __unused)
 {
 	uint32_t expected_ptypes =
 		TEE_PARAM_TYPES(TEE_PARAM_TYPE_NONE, TEE_PARAM_TYPE_NONE,
@@ -145,11 +165,11 @@ static TEE_Result invoke_command(void *session __unused, uint32_t cmd,
 		return unregister_itr(ptypes, params);
 	case READ_CHAR:
 		return read_char(ptypes, params);
+	case WRITE_CHARS:
+		return write_chars(ptypes, params);
 	default:
-		break;
+		return TEE_ERROR_NOT_IMPLEMENTED;
 	}
-
-	return TEE_ERROR_NOT_IMPLEMENTED;
 }
 
 pseudo_ta_register(.uuid = PTA_CONSOLE_SPLIT_UUID, .name = PTA_NAME,
