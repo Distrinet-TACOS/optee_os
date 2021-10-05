3acb4fac465011fae5c5ab08fd057ffbce03e437
Tom Van Eyck <tom.vaneyck@telenet.be>
2021-10-05 22:19:17 +0200

    Add controller implementation

---
+++ core/include/drivers/console_split_public.h
@@ -0,0 +1,36 @@
+#ifndef CONSOLE_SPLIT_PUBLIC
+#define CONSOLE_SPLIT_PUBLIC
+
+#define PTA_NAME "Secure world split driver"
+#define UUID1 0x661b512b
+#define UUID2 0x53a3
+#define UUID3 0x4cec
+#define UUID4 0xa8
+#define UUID5 0xfe
+#define UUID6 0x48
+#define UUID7 0x0c
+#define UUID8 0x8a
+#define UUID9 0x74
+#define UUID10 0x05
+#define UUID11 0xfe
+
+enum con_split_command {
+	READ_CHAR,
+	REGISTER_ITR,
+	UNREGISTER_ITR,
+	WRITE_CHARS,
+	ENABLE_NOTIF,
+	DISABLE_NOTIF,
+	UPDATE_BUFFER
+};
+
+#define BUFFER_SIZE 256
+
+struct update_buffer {
+	char buf[256];
+	unsigned int count;
+	uint32_t sess_id;
+	struct update_buffer *next;
+};
+
+#endif /* CONSOLE_SPLIT_PUBLIC */
